3f82e055bc581b2669bd0935e1117bdacf1c4cb5
Ricardo Ruiz <gaanzz11@gmail.com>
2020-10-08 18:05:04 -0500

    Initial commit

---
+++ src/dicts.ts
@@ -0,0 +1,14 @@
+export const merida = {
+  o: 'p',
+  t: 'r',
+  m: 'n',
+  v: 'b',
+  w: 'q',
+  l: 'k',
+  p: 'P',
+  r: 'R',
+  n: 'N',
+  b: 'B',
+  q: 'Q',
+  k: 'K',
+};
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { merida } from './dicts';
+
+function meridatofen(input: string) {
+  var top = `!""""""""#\n`;
+  var bottom = '/(((((((()';
+
+  for (let line = 8; line >= 1; line--) {
+    var init = '$';
+    for (let column = 0; column < 8; column++) {
+      if (line % 2 === 0) {
+      }
+    }
+  }
+}
